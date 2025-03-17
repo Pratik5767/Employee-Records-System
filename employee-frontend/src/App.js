d87218c55d1a2ddfec20244d0498355fb2bee379
Pratik5676 <pratikvsalunkhe924@gmail.com>
2025-03-17 20:49:00 +0530

    installing react router and created header component

--- employee-frontend/src/App.js
+++ employee-frontend/src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
+import Header from './pages/header/Header';
 
 function App() {
     return (
-        <div>
-            <h1 class="text-center">Hello world</h1>
-        </div>
+        <>
+            <Header/>   
+        </>
     );
 }
 
