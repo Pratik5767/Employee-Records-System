e81ab9a789a42110153cdf6db77e5d064e14af09
Pratik5676 <pratikvsalunkhe924@gmail.com>
2025-03-17 23:17:10 +0530

    dashboard and 404 notfound route created

--- employee-frontend/src/App.js
+++ employee-frontend/src/App.js
@@ -1,10 +1,17 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Dashboard from './pages/dashboard/Dashboard';
 import Header from './pages/header/Header';
+import NoMatch from './pages/noMatch/NoMatch';
 
 function App() {
     return (
         <>
-            <Header/>   
+            <Header />
+            <Routes>
+                <Route path='/' element={<Dashboard />} />
+                <Route path='*' element={<NoMatch />} />
+            </Routes>
         </>
     );
 }
